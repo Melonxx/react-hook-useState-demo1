bcea5785b84661fbf5fd9a015f64919b59b5b331
zhangchao <529743595@qq.com>
2019-06-14 11:37:22 +0800

    使用useEffect API ，但是还是不明白为什么一定要写在这个api里面，写在外面也能达到同样的效果

--- src/card.js
+++ src/card.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Card() {
   const [state, setState] = useState({
     n: 0,
     des: "这是一段描述，只是为了多增加一个属性而已"
   });
+  useEffect(() => {
+    document.querySelector("#myDiv").innerHTML = state.des;
+  });
+  // document.querySelector("#myDiv").innerHTML = state.des + "1231313";
   const add = () => {
     setState({
       ...state,
