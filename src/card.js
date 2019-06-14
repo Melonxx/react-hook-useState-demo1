d0a2217deca5ff57c48952fed9fc223e6595ce0b
zhangchao <529743595@qq.com>
2019-06-14 11:15:54 +0800

    为useState多增加一个属性

--- src/card.js
+++ src/card.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 
 function Card() {
-  const [state, setState] = useState({ n: 0 });
+  const [state, setState] = useState({
+    n: 0,
+    des: "这是一段描述，只是为了多增加一个属性而已"
+  });
   const add = () => {
     setState({
+      ...state,
       n: state.n + 1
     });
   };
   return (
     <div>
+      {state.des}
+      <br />
       {state.n}
+      <br />
       <input onClick={add} value="add" type="button" />
     </div>
   );
